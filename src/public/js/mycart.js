f96a92d135e766a3c2c3b97b722aaf4b65885e8d
hideonbush0512@gmail.com <hideonbush0512@gmail.com>
2022-04-16 22:32:42 +0700

    cart

---
+++ src/public/js/mycart.js
@@ -0,0 +1,20 @@
+// setInterval(function(require){
+					
+//     import {Pool} from 'pg';
+//     // const {migrate} = require('postgres-migrations');
+//     // const env = require('dotenv');
+
+//     env.config({
+//         path:'./.env'
+//     })
+//     const pool = new Pool({
+//         connectionString: process.env.DATABASE_URL,
+//     })
+
+//     pool.connect(function(err, client, done){
+//         if (err) throw err;
+//         console.log('connected!')
+//     });
+//     console.log('Hello mấy cưng')
+//     // document.querySelector('#sum').innerHTML = "Số kí tự đã nhập " + document.querySelector('#mess').value.length;
+// }, 100);
